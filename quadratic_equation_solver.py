2f6e60aa27323a5d1cddb3252d5609e8104b88e7
Max Matveev <109095311+freyais30@users.noreply.github.com>
2023-04-18 12:02:29 +0300

    Create quadratic_equation_solver.py

---
+++ quadratic_equation_solver.py
@@ -0,0 +1,9 @@
+import cmath
+
+a = float(input("Введите значение a: "))
+b = float(input("Введите значение b: "))
+c = float(input("Введите значение c: "))
+
+d = (b**2) - (4*a*c)
+
+root1 = (-b - cmath
